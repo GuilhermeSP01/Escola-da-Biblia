548c4ebfbced0f8966adadcaa2c76d7a9fbb4d1c
Guilherme Silva <133605425+GuilhermeSP01@users.noreply.github.com>
2024-11-16 19:56:56 -0300

    add routes and tailwind

---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline"> Escola da Bíblia </h1>
+      <p> Bem-vindo à Escola da Bíblia </p>
+    </div>
+  )
+}
